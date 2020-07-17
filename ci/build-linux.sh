ca3b3837470cd74b022fcfb238fc718984c2dfa8
Ciprian <ciliesj@gmail.com>
2020-07-16 22:04:11 -0700

    * Modified linux CI build script to use password.

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -2,8 +2,17 @@
 
 # 1) Fetch the dependencies
 
+if [$TESTLIB_PASSWORD -eq ""]; then
+	echo "Error! No password provided for TESTLIB zip. Aborting."
+	exit 1
+fi
+
 mkdir ../dependencies
-export TESTLIB_DIR=$(pwd)/../dependencies/
+cd ..
+export TESTLIB_DIR=$(pwd)/dependencies/
+cd dependencies
+cp ../../SampleGithubCI-Deps/testlib.zip ./
+unzip -P $TESTLIB_PASSWORD testlib.zip
 
 # 2) Build from source
 
@@ -12,12 +21,12 @@ cd ../build/release
 
 cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../..
 if [ $RESULT -ne 0 ]; then
-  echo "CMAKE generation failed, aborting."
+  echo "CMAKE generation failed. Aborting."
   exit 1
 fi
 
 make -j
 if [ $RESULT -ne 0 ]; then
-  echo "Compilation failed, aborting."
+  echo "Compilation failed. Aborting."
   exit 1
 fi
